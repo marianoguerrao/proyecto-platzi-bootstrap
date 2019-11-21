179e24d5483a0b075eca1d0dde9b85b8c12af5f3
marianoguerrao <57159478+marianoguerrao@users.noreply.github.com>
2019-11-20 19:56:41 -0600

    Primer commit ejercicio bootstrap platzi

---
+++ Platzi-bootstrap/index.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip()
+/*function indica que la función se ejecute sólo cuando la página se cargue completamente. 
+data-toggle=tooltip indica que se ejecuten todos lo elementos que tengan este atributo. .tooltip habilita esta palabra*/
+})
